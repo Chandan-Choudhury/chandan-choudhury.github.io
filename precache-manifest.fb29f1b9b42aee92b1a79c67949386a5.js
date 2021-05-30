c2ccbb66fb74b8a756af8e8b74909cf39cb9d989
CHANDAN CHOUDHURY <chandanchoudhury0809@gmail.com>
2021-05-30 12:24:35 +0530

    Updates

---
+++ precache-manifest.fb29f1b9b42aee92b1a79c67949386a5.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed31350af475ebfbd4d2894a3dd0169a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4672e95e8e8ecb8dac56",
+    "url": "/static/css/main.877ad5fa.chunk.css"
+  },
+  {
+    "revision": "453954feabd6efbf2ebe",
+    "url": "/static/js/2.35a501fc.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.35a501fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4672e95e8e8ecb8dac56",
+    "url": "/static/js/main.26f5116c.chunk.js"
+  },
+  {
+    "revision": "d253e35f7bb0066aef2a",
+    "url": "/static/js/runtime-main.b1f99243.js"
+  },
+  {
+    "revision": "cd49fac8e8bc1c923f307602947183ca",
+    "url": "/static/media/1.cd49fac8.jpg"
+  },
+  {
+    "revision": "d0f38554d04187ef9818bdb88fa70ef5",
+    "url": "/static/media/1.d0f38554.jpg"
+  },
+  {
+    "revision": "50c1e10f7894e3484e767a1b2297f65a",
+    "url": "/static/media/10.50c1e10f.jpg"
+  },
+  {
+    "revision": "3cd5904d959e183756b8364441e61566",
+    "url": "/static/media/2.3cd5904d.jpg"
+  },
+  {
+    "revision": "a532d92c28b81c5ac54c3321a659223f",
+    "url": "/static/media/2.a532d92c.jpg"
+  },
+  {
+    "revision": "a17a74ef6f8f06226f0924d5832d5394",
+    "url": "/static/media/3.a17a74ef.jpg"
+  },
+  {
+    "revision": "ad5e88d4700a6c5e11870ea9f5fe5a93",
+    "url": "/static/media/3.ad5e88d4.jpg"
+  },
+  {
+    "revision": "24422c1c660058e3feef6b9056c0002d",
+    "url": "/static/media/4.24422c1c.jpg"
+  },
+  {
+    "revision": "69d29c1f19c08573c5a7c74b0c73be80",
+    "url": "/static/media/4.69d29c1f.jpg"
+  },
+  {
+    "revision": "a1c593c2a8ec6b547899f693698e4550",
+    "url": "/static/media/5.a1c593c2.jpg"
+  },
+  {
+    "revision": "bf908424c587616ea96b3c99fb1b8520",
+    "url": "/static/media/5.bf908424.jpg"
+  },
+  {
+    "revision": "93a89bd894dc36b1bc73573df7da8e2f",
+    "url": "/static/media/6.93a89bd8.jpg"
+  },
+  {
+    "revision": "e634ef7bc17afbc5d09143f61bfc4c74",
+    "url": "/static/media/6.e634ef7b.jpg"
+  },
+  {
+    "revision": "4797d8f2e707f48e3b72481c8fc5241d",
+    "url": "/static/media/7.4797d8f2.jpg"
+  },
+  {
+    "revision": "62512da00be8413084c6ef954143d196",
+    "url": "/static/media/7.62512da0.jpg"
+  },
+  {
+    "revision": "8808f3b5f45fa34bdef86c8c66d605fb",
+    "url": "/static/media/8.8808f3b5.jpg"
+  },
+  {
+    "revision": "bcd6f8239adda82201b15815a048790a",
+    "url": "/static/media/8.bcd6f823.jpg"
+  },
+  {
+    "revision": "1d0b35875a4ac09263fea9959ae2b259",
+    "url": "/static/media/9.1d0b3587.jpg"
+  },
+  {
+    "revision": "23ae276ab64a4e219c51b1161cbe0af9",
+    "url": "/static/media/9.23ae276a.jpg"
+  },
+  {
+    "revision": "b3781e27cb8b12c0c60e4e211f7483c7",
+    "url": "/static/media/AerialShots.b3781e27.jpg"
+  },
+  {
+    "revision": "32e5bc1e61a24451e9ca3c0dcf8c78b7",
+    "url": "/static/media/Currency.32e5bc1e.jpg"
+  },
+  {
+    "revision": "0f970a0d691ad8f05940d9bd669e7303",
+    "url": "/static/media/Photography.0f970a0d.jpg"
+  },
+  {
+    "revision": "7a527992e63ee5c770f8f86b83791586",
+    "url": "/static/media/Pro.7a527992.png"
+  },
+  {
+    "revision": "b1cdce826bce2ce7af6233871789def8",
+    "url": "/static/media/graduation.b1cdce82.png"
+  },
+  {
+    "revision": "d62aeb2d1690e1a6336fa3fb685b0bc8",
+    "url": "/static/media/message.d62aeb2d.png"
+  },
+  {
+    "revision": "dd2a291314bae7b4990ffb68af0f438b",
+    "url": "/static/media/video.dd2a2913.mp4"
+  }
+]);
